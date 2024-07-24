4eacac63b6301473c019b8b2a3f55b065d6d0bf7
samuel <samuelromero.fortin@gmail.com>
2024-07-24 14:01:55 +0900

    ButtonBase impl

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase'; 
 
 import AppTasks from '../app-tasks';
 import AppOrderTimeline from '../app-order-timeline';
@@ -24,85 +25,99 @@ export default function AppView() {
         </Grid>
 
         <Grid container spacing={3}>
-        <Grid xs={14} sm={8} md={6}>
-          <AppWidgetSummary
-            title="IP Address"
-            total={1}
-            color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
-        </Grid>
+          <Grid xs={14} sm={8} md={6}>
+            <ButtonBase style={{ width: '100%' }}>
+              <AppWidgetSummary
+                style={{ width: '100%' }}
+                title="IP Address"
+                total={1}
+                color="primary"
+                icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+              />
+            </ButtonBase>
+          </Grid>
+       
 
         <Grid xs={14} sm={8} md={6}>
-          <AppWidgetSummary
-            title="Save Position"
-            total={2}
-            color="info"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
-          />
+          <ButtonBase style={{ width: '100%' }}>
+          <AppWidgetSummary style={{ width: '100%'}}
+              title="Save Position"
+              total={2}
+              color="primary"
+              icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
+            />
+          </ButtonBase>
         </Grid>
 
-        <Grid xs={11} sm={6} md={4}>
-          <AppWidgetSummary
-            title="Tool 1"
-            total={3}
-            color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
-          />
-        </Grid>
+          <Grid item xs={11} sm={6} md={4}>
+            <ButtonBase style={{ width: '100%' }}>
+              <AppWidgetSummary style={{ width: '100%'}}
+              title="Tool 1"
+              total={3}
+              color="primary"
+              icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+            />
+            </ButtonBase>
+          </Grid>
 
-        
-        <Grid xs={11} sm={6} md={4}>
-          <AppWidgetSummary
-            title="Tool 2"
-            total={4}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
-
-        <Grid xs={11} sm={6} md={4}>
-          <AppWidgetSummary
-            title="Stop"
-            total={5}
-            color="Run Project"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
-
-        <Grid xs={10} md={4} lg={6}>
-          <AppTasks
-            title="Tasks"
-            list={[
-              { id: '1', name: 'Activate robot arm' },
-              { id: '2', name: 'Manual move' },
-              { id: '3', name: 'Use web tools' },
-              { id: '4', name: 'Save positions' },
-              { id: '5', name: 'Run project ' },
-            ]}
-          />
-        </Grid>
+          
+          <Grid item xs={11} sm={6} md={4}>
+            <ButtonBase style={{ width: '100%' }}>
+              <AppWidgetSummary style={{ width: '100%'}}
+              title="Tool 2"
+              total={4}
+              color="primary"
+              icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+            />
+            </ButtonBase>
+          </Grid>
+          
+          
+          <Grid item xs={11} sm={6} md={4}>
+            <ButtonBase style={{ width: '100%' }}>
+              <AppWidgetSummary style={{ width: '100%'}}
+                title="Stop"
+                total={5}
+                color="primary"
+                icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+              />
+            </ButtonBase>
+          </Grid>
 
 
-        <Grid xs={13} md={7} lg={6}>
-          <AppOrderTimeline
-            title="Process"
-            list={[...Array(3)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                'Connect to robot arm',
-                'Move and Save different positions',
-                'Execute your robot automation project',
-              ][index],
-              type: `order${index + 1}`,
-              text: [
-                ['2 min aprox'],
-                ['4 min aprox'],
-                ['1 sec'],
-              ][index],
-            }))}  
-          />
-        </Grid>
+          <Grid xs={10} md={4} lg={6}>
+            <AppTasks
+              title="Tasks"
+              list={[
+                { id: '1', name: 'Activate robot arm' },
+                { id: '2', name: 'Manual move' },
+                { id: '3', name: 'Use web tools' },
+                { id: '4', name: 'Save positions' },
+                { id: '5', name: 'Run project ' },
+              ]}
+            />
+          </Grid>
+
+
+          <Grid xs={13} md={7} lg={6}>
+            <AppOrderTimeline
+              title="Process"
+              list={[...Array(3)].map((_, index) => ({
+                id: faker.string.uuid(),
+                title: [
+                  'Connect to robot arm',
+                  'Move and Save different positions',
+                  'Execute your robot automation project',
+                ][index],
+                type: `order${index + 1}`,
+                text: [
+                  ['2 min aprox'],
+                  ['4 min aprox'],
+                  ['1 sec'],
+                ][index],
+              }))}  
+            />
+          </Grid>
 
 
 
