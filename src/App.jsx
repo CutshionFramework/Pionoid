bbad4bdae1e52dd113b0b4ee052de1df04fec531
junhui324 <junhuijang00@gmail.com>
2024-08-05 17:53:54 +0900

    Fix: App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-/* eslint-disable perfectionist/sort-imports */
-import 'src/global.css';
+import './global.css';
+import { useScrollToTop } from './hooks/use-scroll-to-top';
+import Router from './routes/sections';
+import ThemeProvider from './theme';
 
-import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
-
-import Router from 'src/routes/sections';
-import ThemeProvider from 'src/theme';
-
-// ----------------------------------------------------------------------
+// --------------------------------
 
 export default function App() {
   useScrollToTop();
