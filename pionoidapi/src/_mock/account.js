68f09faf3533155c786a61584154fa643fb58cff
samuel <samuelromero.fortin@gmail.com>
2024-08-06 09:31:35 +0900

    folder struct

--- pionoidapi/src/_mock/account.js
+++
@@ -1,7 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: 'User',
-  email: 'cutshion@cutshion.com',
-  photoURL: '/assets/images/avatars/avatar_25.jpg',
-};
