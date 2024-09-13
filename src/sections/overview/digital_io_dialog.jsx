47fb9ae9d42aa881550312f061954873e8066b1c
junhui324 <junhuijang00@gmail.com>
2024-09-13 15:00:34 +0900

    Fix: Add Collapse in IODialog

--- src/sections/overview/digital_io_dialog.jsx
+++ src/sections/overview/digital_io_dialog.jsx
@@ -9,15 +9,27 @@ import {
   Button,
   Tabs,
   Tab,
+  Collapse,
+  IconButton,
 } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { getIOStatus, setIO } from '../../apis/apis';
 
 const statusStyle = (isActive) => css`
   padding: 10px;
-  border-radius: 5px;
+  border-radius: 10px;
   background-color: ${isActive ? '#71f49a' : '#efedec'};
   color: black;
   text-align: center;
+`;
+
+const buttonStyle = css`
+  display: inline-block;
+  margin: 4px;
+`;
+
+const dialogContentStyle = css`
+  min-height: 400px; /* Adjust as needed */
 `;
 
 function IODialog({ open, onClose }) {
@@ -27,6 +39,15 @@ function IODialog({ open, onClose }) {
     Extend: { extio: [], out: [], in: [] },
   });
   const [currentTab, setCurrentTab] = useState(0);
+  const [openSections, setOpenSections] = useState({
+    DO: false,
+    DI: false,
+    AO: false,
+    AI: false,
+    extio: false,
+    out: false,
+    in: false,
+  });
   const buttons = Array.from({ length: 16 }, (_, i) => i + 1); // 고정된 버튼 배열
   const tabNames = ['Cabinet', 'Tool', 'Extend'];
   const title = tabNames[currentTab];
@@ -49,20 +70,17 @@ function IODialog({ open, onClose }) {
 
   const updateButtonStates = (data) => {
     const updatedButtons = { ...activeButtons };
-    // Cabinet
     updatedButtons.Cabinet = {
       DO: data.CABINET.dout[0].slice(0, 16).map((val) => val === 1),
       DI: data.CABINET.din[0].slice(0, 16).map((val) => val === 1),
       AO: data.CABINET.aout[0].slice(0, 16).map((val) => val === 1),
       AI: data.CABINET.ain[0].slice(0, 16).map((val) => val === 1),
     };
-    // Tool
     updatedButtons.Tool = {
       DO: data.TOOL.tio_dout[0].slice(0, 2).map((val) => val === 1),
       DI: data.TOOL.tio_din[0].slice(0, 2).map((val) => val === 1),
       AI: data.TOOL.tio_ain[0].slice(0, 2).map((val) => val === 1),
     };
-    // Extend
     updatedButtons.Extend = {
       extio: data.EXTEND.extio.length ? [data.EXTEND.extio[0] === 'extio'] : [],
       out: data.EXTEND.out.slice(0, 16).map((val) => val === 1),
@@ -77,11 +95,10 @@ function IODialog({ open, onClose }) {
 
   const handleButtonClick = async (button, type) => {
     const tabName = tabNames[currentTab];
-    const buttonIndex = button - 1; // Convert to zero-based index
+    const buttonIndex = button - 1;
     const updatedButtons = { ...activeButtons };
     const isActive = updatedButtons[tabName][type].includes(buttonIndex);
 
-    // Toggle the button state
     if (isActive) {
       updatedButtons[tabName][type] = updatedButtons[tabName][type].filter(
         (b) => b !== buttonIndex
@@ -93,13 +110,10 @@ function IODialog({ open, onClose }) {
       ];
     }
 
-    // Set the new state
     setActiveButtons(updatedButtons);
 
-    // Determine the value to send ('on' or 'off')
     const value = isActive ? 0 : 1;
 
-    // Send the IO status to the server
     try {
       await setIO(tabName, type, buttonIndex, value);
     } catch (error) {
@@ -107,28 +121,48 @@ function IODialog({ open, onClose }) {
     }
   };
 
+  const toggleSection = (type) => {
+    setOpenSections((prev) => ({ ...prev, [type]: !prev[type] }));
+  };
+
   const renderButtons = (types) => {
     return types.map((type) => (
       <React.Fragment key={type}>
         <Grid item xs={12}>
-          <h3>{type}</h3>
+          <h3>
+            {type}
+            <IconButton onClick={() => toggleSection(type)}>
+              <ExpandMoreIcon
+                style={{
+                  transform: openSections[type]
+                    ? 'rotate(0deg)'
+                    : 'rotate(180deg)',
+                }}
+              />
+            </IconButton>
+          </h3>
         </Grid>
-        {buttons.map((button) => (
-          <Grid item xs={12} sm={6} md={4} key={button}>
-            <Button
-              variant={
-                activeButtons[tabNames[currentTab]][type]?.includes(button - 1)
-                  ? 'contained'
-                  : 'outlined'
-              }
-              color="primary"
-              fullWidth
-              onClick={() => handleButtonClick(button, type)}
-            >
-              {`${type} ${button}`}
-            </Button>
+        <Collapse in={openSections[type]} timeout="auto" unmountOnExit>
+          <Grid container spacing={1}>
+            {buttons.map((button) => (
+              <Grid item key={button} css={buttonStyle}>
+                <Button
+                  variant={
+                    activeButtons[tabNames[currentTab]][type]?.includes(
+                      button - 1
+                    )
+                      ? 'contained'
+                      : 'outlined'
+                  }
+                  color="primary"
+                  onClick={() => handleButtonClick(button, type)}
+                >
+                  {`${type} ${button}`}
+                </Button>
+              </Grid>
+            ))}
           </Grid>
-        ))}
+        </Collapse>
       </React.Fragment>
     ));
   };
@@ -137,29 +171,46 @@ function IODialog({ open, onClose }) {
     return types.map((type) => (
       <React.Fragment key={type}>
         <Grid item xs={12}>
-          <h3>{type}</h3>
+          <h3>
+            {type}
+            <IconButton onClick={() => toggleSection(type)}>
+              <ExpandMoreIcon
+                style={{
+                  transform: openSections[type]
+                    ? 'rotate(0deg)'
+                    : 'rotate(180deg)',
+                }}
+              />
+            </IconButton>
+          </h3>
         </Grid>
-        {activeButtons[tabNames[currentTab]][type].map((isActive, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <div css={statusStyle(isActive)}>
-              {`${type} ${index + 1} : `}
-              {isActive ? 'Active' : 'Inactive'}
-            </div>
+        <Collapse in={openSections[type]} timeout="auto" unmountOnExit>
+          <Grid container spacing={1}>
+            {activeButtons[tabNames[currentTab]][type].map(
+              (isActive, index) => (
+                <Grid item key={index} css={buttonStyle}>
+                  <div css={statusStyle(isActive)}>
+                    {`${type} ${index + 1} : `}{' '}
+                    {isActive ? 'Active' : 'Inactive'}
+                  </div>
+                </Grid>
+              )
+            )}
           </Grid>
-        ))}
+        </Collapse>
       </React.Fragment>
     ));
   };
 
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth="md">
       <DialogTitle>{title}</DialogTitle>
       <Tabs value={currentTab} onChange={handleTabChange} centered>
         <Tab label="Cabinet" />
         <Tab label="Tool" />
         <Tab label="Extend" />
       </Tabs>
-      <DialogContent>
+      <DialogContent css={dialogContentStyle}>
         <Grid container spacing={2}>
           {currentTab === 0 && (
             <>
